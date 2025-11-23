cb1259d67eb82cc6bddb742a0f24a77cd28c9dbd
THEGODARJUN42 <arjunraghav0109@gmail.com>
2025-11-23 19:35:57 +0530

    Add files via upload

---
+++ acp.2.py
@@ -0,0 +1,16 @@
+class Circle:
+
+    def__init__(self,radius)
+      self.radius = radius
+    
+    def perimeter(self):
+       return 3.14 * self.radius * self.radius
+    
+    def perimeter(self)
+       return 2 * 3.14 * self.radius
+    
+
+r = float(input("Enter the radius of a circle" ))
+circle1 = Circle(r)
+
+print("Perimeter of a circle", circle1.perimeter())
